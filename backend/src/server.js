6ef6e88d2652ef45aeef36344d76b490b9f0ad63
Taher-22 <mah.mohamed@nu.edu.eg>
2026-01-17 05:09:52 +0200

    Minimal server test

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send("OK - express + json");
+  res.status(200).send("OK - cors enabled");
 });
 
 const PORT = 8080;
