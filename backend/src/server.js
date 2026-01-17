bf46fe38b311bc35b6e195db2c9165fbb44e6ecf
Taher-22 <mah.mohamed@nu.edu.eg>
2026-01-17 05:05:32 +0200

    Minimal server test

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.status(200).send("OK - minimal server");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log("MINIMAL SERVER LISTENING ON", PORT);
