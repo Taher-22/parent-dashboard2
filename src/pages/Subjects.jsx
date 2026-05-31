0b03cb37cd1dc8c9911312800892a04d68548cd0
Mohamed Nasr <nasrrtm@gmail.com>
2026-05-31 17:55:30 +0300

    Subjects: fit 4 cards in one clean row (2x2 tablet, 4-up desktop)

--- src/pages/Subjects.jsx
+++ src/pages/Subjects.jsx
@@ -45,7 +45,7 @@ export default function Subjects() {
       </div>
 
       {/* SUBJECT GRID */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 mt-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
         {subjects.map((s) => (
           <motion.div
             key={s.id}
